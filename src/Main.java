1477fae40815a6ac82969a250cb8adb8e03a07df
Plichtic <queplichtic@gmail.com>
2024-05-12 10:30:43 +0200

    Dijkstr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import GraphPackage.Graph;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+
+
+        Graph graph = new Graph();
+
+        graph.loadGraph();
+        graph.getGraph();
+        System.out.println(graph.dijkstras(0,7));
     }
+
 }
